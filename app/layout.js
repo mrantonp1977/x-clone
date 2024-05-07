59453447e20fdbe4c0c2190c64f11b4f18f94680
Antonis Papaioannou <mrantonp@gmail.com>
2024-05-07 18:48:50 +0300

    complete news section

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,14 @@ export default function RootLayout({ children }) {
           <div>
             {children}
           </div>
-          <div>
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className="sticky top-0 bg-white py-2">
+              <input 
+                type="text" 
+                placeholder='Search...'
+                className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </div>
             <News />
           </div>
         </div>
